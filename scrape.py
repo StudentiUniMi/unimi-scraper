8135ab12880a2b48abae58215dd4cad1d4fa23c9
mroik <mroik@delayed.space>
2021-08-21 17:35:02 +0200

    Fixed department data
    It previously showed the faculty instead of the department

--- scrape.py
+++ scrape.py
@@ -8,12 +8,19 @@ def parse_data(data):
     for corso in data:
         for x in corso.div.div.div.div["class"]:
             if x.startswith("areacard"):
-                dip = DIPARTIMENTI[x]
+                fac = FACOLTA[x]
     
         content = corso.div.div.div.div.div.find(class_="card-content")
         content.find(class_="card_center").find(class_="sede").clear()
+        dip = content.find(class_="card-bottom").div.div.a["href"].strip()
+
+        # The only case that enters in this branch is with the course of AI, it is on "unipv.it"
+        if "unimi.it" not in dip:
+            dip = None
+
         result.append({
-            "dipartimento": dip,
+            "facolta": fac,
+            "dipartimento": get_dipartimento(dip),
             "anno": content.find(class_="anno-accademico").string,
             "corso": content.find(class_="bp-title").a.string,
             "sito": "https://unimi.it" + content.find(class_="bp-title").a["href"],
@@ -21,14 +28,33 @@ def parse_data(data):
             "lingua": content.find(class_="card_center").get_text().replace("\n", "").strip()
         })
     return result
-    
+
+
+def get_dipartimento(link):
+    if link is None:
+        return ""
+    resp = requests.get(
+            link,
+            headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"}
+    )
+    if resp.status_code != 200:
+        raise Exception("Couldn't fetch the page")
+    data = bs(resp.text, "lxml")
+    dip = data.find(class_="bgcolor-PRINCIPALE")
+    if dip is None:
+        dip = data.find(class_="bgcolor-ASSRESPONS")
+    if dip is None:
+        return ""
+    dip = dip.div.div.a.string.strip()
+    return dip
+
 
 TRIENNALE = "https://www.unimi.it/it/corsi/corsi-di-laurea-triennali-e-magistrali-ciclo-unico"
 MAGISTRALE = "https://www.unimi.it/it/corsi/corsi-di-laurea-magistrale"
 INSEGNAMENTI = "https://www.unimi.it/it/corsi/insegnamenti-dei-corsi-di-laurea/insegnamenti-dei-corsi-di-laurea-2021-2022"
 
-# For now each department is mapped this way 'cause I can't be bothered to properly extract data from an external CSS
-DIPARTIMENTI = {
+# For now each faculty is mapped this way 'cause I can't be bothered to properly extract data from an external CSS
+FACOLTA = {
         "areacard-C": "SU",
         "areacard-Z": "SM",
         "areacard-A": "G",
@@ -46,6 +72,8 @@ DIPARTIMENTI = {
 def main():
     # triennale
     resp = requests.get(TRIENNALE)
+    if resp.status_code != 200:
+        raise Exception("Couldn't fetch the page")
     data = bs(resp.text, "lxml").find(class_="view-cercacorsi").find("div", class_="view-content")
     data = data.find_all("div", class_="corso")
     
@@ -57,6 +85,8 @@ def main():
     
     # magistrale
     resp = requests.get(MAGISTRALE)
+    if resp.status_code != 200:
+        raise Exception("Couldn't fetch the page")
     data = bs(resp.text, "lxml").find(class_="view-cercacorsi").find("div", class_="view-content")
     data = data.find_all("div", class_="corso")
 
